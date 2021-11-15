ac76e6a2375d22f7e51f0bcb59518c28e0745ced
kyzooghost <73516204+kyzooghost@users.noreply.github.com>
2021-11-16 01:02:29 +1100

    Pushed canvas to IPFS

--- image_to_ipfs.ts
+++ image_to_ipfs.ts
@@ -1,7 +1,10 @@
+// main() is to savee a pre-existing image file to Moralis IPFS
+// main2() is to save a canvas created within the script context to Moralis IPFS
+
 require("dotenv").config();
 import Moralis from 'moralis/node'
-
 const fs = require("fs");
+const { createCanvas, loadImage } = require('canvas')
 
 const serverUrl = process.env.SERVER;
 const appId = process.env.APP_ID;
@@ -12,19 +15,34 @@ Moralis.start({ serverUrl, appId, masterKey });
 const imagePath = './0.png'
 let file: any;
 
-const promise = new Promise((res, rej) => {
-  fs.readFile(imagePath, (err: any, data: any) => {
-    if(err) rej();
-    const image = "data:image/png;base64," + data.toString("base64")
-    file = new Moralis.File("image.png", {base64 : image });
-    res(null);
-  })
-})
+// const promise = new Promise((res, rej) => {
+//   fs.readFile(imagePath, (err: any, data: any) => {
+//     if(err) rej();
+//     const image = "data:image/png;base64," + data.toString("base64")
+//     file = new Moralis.File("image.png", {base64 : image });
+//     res(null);
+//   })
+// })
 
-const main = async () => {
-  await promise
+// const main = async () => {
+//   await promise
+//   await file.saveIPFS({ useMasterKey: true });
+//   console.log(file.ipfs(), file.hash())
+// }
+
+// main()
+
+const main2 = async() => {
+  const canvas = createCanvas(2048, 2048)
+  const ctx = canvas.getContext('2d')
+  ctx.clearRect(0, 0, 2048, 2048);
+  ctx.fillStyle = "#FFFFFF";
+  ctx.fillRect(0, 0, 2048, 2048);
+  const raw_image = canvas.toBuffer("image/png")
+  const raw_image_2 = "data:image/png;base64," + raw_image.toString("base64")
+  file = new Moralis.File("image.png", {base64 : raw_image_2 });
   await file.saveIPFS({ useMasterKey: true });
-  console.log(file.ipfs(), file.hash())
+  console.log(file.ipfs(), file.hash())  
 }
 
-main()
+main2()
